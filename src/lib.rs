d539434b4d0d34ddb59111615a267b9b0a757224
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-04-27 21:09:10 +1000

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,64 @@
+#![warn(missing_docs)]
+//! A small genetic algorithms library.
 use rand::{seq::SliceRandom, Rng};
 use rayon::prelude::*;
 
+/// An interface for breeding, mutation, and fitness evaluation functionality.
 pub trait Organism {
+    /// Evaluates the organism's fitness.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// impl Organism for Bird {
+    ///     fn fitness(&self) -> f64 {
+    ///         self.x.powi(2)
+    ///     }
+    /// }
+    /// ```
     fn fitness(&self) -> f64;
+
+    /// Creates a new child by breeding the organism with another.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// impl Organism for Bird {
+    ///     fn breed(&self, other: &Self) -> Self {
+    ///         Self {
+    ///             x: 0.0,
+    ///             flap_power: (self.flap_power + other.flap_power) / 2.0,
+    ///         }
+    ///     }
+    /// }
+    /// ```
     fn breed(&self, other: &Self) -> Self;
+
+    /// Modifies (or *mutates*) the organism, based on the given rate.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use rand::Rng;
+    ///
+    /// impl Organism for Bird {
+    ///     fn mutate(&mut self, rate: f64) {
+    ///         let change = rand::thread_rng().gen_range(-1.0, 1.0);
+    ///         self.flap_power += change * rate;
+    ///     }
+    /// }
+    /// ```
     fn mutate(&mut self, rate: f64);
 }
 
+/// A collection of organisms.
 pub struct Ecosystem<O: Organism> {
     organisms: Vec<O>,
     generation: u32,
 }
 
 impl<O: Organism + std::marker::Send + std::marker::Sync> Ecosystem<O> {
+    /// Creates a new ecosystem with the given organisms.
     pub fn new(organisms: Vec<O>) -> Self {
         Self {
             organisms,
@@ -20,6 +66,7 @@ impl<O: Organism + std::marker::Send + std::marker::Sync> Ecosystem<O> {
         }
     }
 
+    /// Returns the organism in the ecosystem with the highest fitness.
     pub fn fittest(&self) -> &O {
         self.organisms
             .iter()
@@ -32,6 +79,8 @@ impl<O: Organism + std::marker::Send + std::marker::Sync> Ecosystem<O> {
             })
     }
 
+    /// Creates the next generation of organisms through the breeding
+    /// of suitable organisms.
     pub fn breed_next_generation(&mut self, mutation_rate: f64) {
         let next_generation: Vec<_> = (0..self.organisms.len())
             .into_par_iter()
@@ -49,6 +98,12 @@ impl<O: Organism + std::marker::Send + std::marker::Sync> Ecosystem<O> {
         self.generation += 1;
     }
 
+    /// Selects an organism in the ecosystem that is suitable for breeding,
+    /// based on fitness values.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if the ecosystem contains no organisms.
     fn select_suitable_organism(&self) -> &O {
         let mut rng = rand::thread_rng();
         loop {
